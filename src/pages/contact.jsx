46110a4533e0540b89f75e8fa9d7d7b567553eb8
Shashanka <poudelshashanka205@gmail.com>
2025-01-30 13:42:59 +0545

    latest changes

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -76,7 +76,7 @@ const Contact = () => {
                             </a>
                         </div>
 
-                    </div>
+                    </div> 
 
                     <div className='flex flex-col  mt-4'>
                         <p className='text-2xl font-playfair-display text-start mr-auto font-semibold text-[#03385F]'>Social media links:</p>
@@ -86,7 +86,7 @@ const Contact = () => {
                             <a href='https://www.instagram.com/soundhealing.hk/'><FaInstagram className='text-2xl text-[#E4405F]' /></a>
                             <a href='https://www.youtube.com/@Yogi_akash/featured '><FaYoutube className=' text-2xl text-[#FF0000]' /></a>
                         </div>
-                        
+
                     </div>
 
                 </div>
@@ -98,7 +98,7 @@ const Contact = () => {
                 </div>
 
             </div>
-            {/* <SocialCard /> */}
+          
         </div>
 
     );
