b3890feb3727e234bbda3d583d993c9759566b6b
Shashanka <poudelshashanka205@gmail.com>
2025-08-17 12:17:42 +0545

    latest changes

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,6 +1,6 @@
 const BASE_URL =
     import.meta.env.MODE === "development"
         ? "http://localhost:8000/api"
-        : "";
+        : "https://master-akash-backend-1.onrender.com/api";
 
 export default BASE_URL;
